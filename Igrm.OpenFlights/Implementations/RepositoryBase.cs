60bf6b44675ccf16d11a67f8658560726bb0e8b1
igrm <igor.mishur@gmail.com>
2019-09-09 14:24:51 +0300

    Initial project structure

---
+++ Igrm.OpenFlights/Implementations/RepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Igrm.OpenFlights.Implementations
+{
+    class RepositoryBase
+    {
+    }
+}
---
+++ Igrm.OpenFlights/OpenFlightsDataCache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Igrm.OpenFlights
+{
+    public class OpenFlightsDataCache : IOpenFlightsDataCache
+    {
+    }
+}
