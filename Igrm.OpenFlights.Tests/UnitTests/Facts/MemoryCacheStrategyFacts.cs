e8b48dcb3bb939605fa770c069385696cd75117c
igrm <igor.mishur@gmail.com>
2019-09-12 17:02:03 +0300

    Unit tests added

---
+++ Igrm.OpenFlights.Tests/UnitTests/Facts/MemoryCacheStrategyFacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Igrm.OpenFlights.Interfaces;
+using Igrm.OpenFlights.Models;
+using System.Threading.Tasks;
+using Igrm.OpenFlights.Implementations;
+
+namespace Igrm.OpenFlights.Tests.UnitTests.Facts
+{
+    public class MemoryCacheStrategyFacts
+    {
+        public class PostEvictionDelegateTests
+        {
+            [Fact]
+            public void WhenCacheExpired_FileLoadIsCalled()
+            {
+                //ARRANGE
+                bool fileLoaded = false;
+                var dataFileLoader = new Mock<IDataFileLoader>(MockBehavior.Loose);
+                dataFileLoader.Setup(x => x.LoadFileAsync<AircraftsList>(true)).Returns(Task.Run(() => { fileLoaded = true; }));
+                //ACT
+                ICacheStrategy<AircraftsList> cache = new MemoryCacheStrategy<AircraftsList>(1, dataFileLoader.Object);
+                cache.ExecuteSetAsyn(new AircraftsList(), "Planes").Wait();
+                Task.Delay(70000).Wait();
+                //ASSERT
+                Assert.True(fileLoaded);
+            }
+        }
+    }
+}
